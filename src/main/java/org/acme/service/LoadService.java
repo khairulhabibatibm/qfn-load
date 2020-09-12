e9e93279279ca4310bc8279f6695e320680fb5d5
Khairul Habib <mkhairulhabibm@gmail.com>
2020-09-12 09:18:30 +0700

    add external config

--- src/main/java/org/acme/service/LoadService.java
+++ src/main/java/org/acme/service/LoadService.java
@@ -1,5 +1,7 @@
 package org.acme.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.stream.Stream;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -8,16 +10,54 @@ import javax.inject.Inject;
 import org.acme.persistence.ProcurService;
 import org.acme.persistence.entity.ProcurementEntity;
 
+import io.agroal.api.AgroalDataSource;
+import io.quarkus.agroal.DataSource;
+
 @ApplicationScoped
 public class LoadService {
     
     @Inject
     ProcurService service;
 
+    @Inject
+    @DataSource("pg")
+    AgroalDataSource stagingDs;
+
     public void readAllData(){
-        Stream<ProcurementEntity> stream = service.getAllData();
-        stream.forEach(d -> {
-            System.out.println(d.getId() + ":" + d.getCostCenter() + ":" + d.getNamaAnggaran());
-        });
+        Connection conn = null;
+        try {
+            conn = stagingDs.getConnection();
+            Stream<ProcurementEntity> stream = service.getAllData();
+
+            PreparedStatement ps = conn.prepareStatement("insert into public.proc_staging values (?,?,?,?)");
+            stream.forEach(d -> {
+                try {
+                    // System.out.println(d.getId() + ":" + d.getCostCenter() + ":" + d.getNamaAnggaran());
+
+                    ps.setLong(1, d.getId());    
+                    ps.setString(2, d.getBudgetName());
+                    ps.setString(3, d.getCostCenter());
+                    ps.setString(4, d.getNamaAnggaran());
+
+                    ps.addBatch();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                    e.printStackTrace();
+                }
+            });    
+            ps.executeBatch();
+        } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                //TODO: handle exception
+                e.printStackTrace();
+            }
+        }
+        
     }
+
 }
